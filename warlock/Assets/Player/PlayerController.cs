16b291c379da51e1a8928e36d4a2ae2b089c1fd9
Phenix <vlt31@hotmail.fr>
2019-04-03 22:31:50 +0200

    Spell first Setup
    Setup various spell-related classes.
    Theorically, a spellFactory create spells and spellCasts to add to each player object.

---
+++ warlock/Assets/Player/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    SpellCast[] spells = new SpellCast[5];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
---
+++ warlock/Assets/Spells/SpellCast.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpellCast : MonoBehaviour
+{
+    float cooldown;
+    Image icon;
+    GameObject cursorImage;
+    GameObject spellToCast;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+    GameObject Cast()
+    {
+        GameObject obj = Instantiate(spellToCast);
+        return obj;
+    }
+}
