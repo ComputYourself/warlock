fdf3e9ba9b10980f46606c8c2c4a46856927b63c
Phenix <vlt31@hotmail.fr>
2019-04-24 23:25:23 +0200

    Network init
    Network establishment, NetworkEvent implementation.

---
+++ warlock/Assets/Network/NonBehaviours/NetworkEvent.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class NetworkEvent
+{
+    // Constructors
+
+    // Creators
+    public static T Create<T>(byte[] data)
+        where T : NetworkEvent, new()
+    {
+        T ne = new T();
+        if (!ne.Deserialize(data))
+            return null;
+
+        return ne;
+    }
+
+    public static T Create<T>()
+        where T : NetworkEvent, new()
+    {
+        T ne = new T();
+        return ne;
+    }
+
+    public virtual byte[] Serialize()
+    {
+        return new byte[0];
+    }
+
+    public virtual bool Deserialize(byte[] data)
+    {
+        return data.Length == 0;
+    }
+}
+
+//Exemple
+public class NetworkEvent_ExempleVide : NetworkEvent
+{
+    
+}
+
+
